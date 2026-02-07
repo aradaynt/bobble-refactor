dfc8e375be4a381768c8e12e0591521edf626b5b
Arad Ayntabli <arad.ayntabli@ontariotechu.net>
2026-02-07 14:34:09 -0500

    Added the pause class

---
+++ src/screens/pause.py
@@ -0,0 +1,14 @@
+from src.input import InputState
+
+class PauseScreen:
+    def __init__(self, app, play_screen):
+        self.app = app
+        self.play_screen = play_screen
+
+    def update(self, input_state: InputState):
+        if input_state.pause_pressed:
+            self.app.resume_play(self.play_screen)
+
+    def draw(self):
+        self.play_screen.draw()
+        screen.draw.text("PAUSED", center=(400, 240), fontsize=60, color="white", owidth=1.5, ocolor="black")

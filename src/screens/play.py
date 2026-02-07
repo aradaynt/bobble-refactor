895d350eca899e11d0c36c36a0a66e500e8e37c8
Arad Ayntabli <arad.ayntabli@ontariotechu.net>
2026-02-07 14:09:04 -0500

    Added play class

---
+++ src/screens/play.py
@@ -0,0 +1,17 @@
+from src.game import Game
+from src.input import InputState
+
+class PlayScreen:
+    def __init__(self, app):
+        self.app = app
+        from src.game import Player 
+        self.game = Game(Player())
+
+    def update(self, input_state: InputState):
+        if self.game.player.lives < 0:
+            self.app.switch_to_game_over()
+        else:
+            self.game.update(input_state)
+
+    def draw(self):
+        self.game.draw()

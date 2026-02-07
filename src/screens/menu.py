06a221723988b5bce5bdfef6c591256e45d15033
Arad Ayntabli <arad.ayntabli@ontariotechu.net>
2026-02-07 14:03:46 -0500

    Added the menu class

---
+++ src/screens/menu.py
@@ -0,0 +1,17 @@
+from src.input import InputState
+
+class MenuScreen:
+    def __init__(self, app):
+        self.app = app
+        self.timer = 0
+
+    def update(self, input_state: InputState):
+        self.timer += 1
+        
+        if input_state.fire_pressed:
+            self.app.switch_to_play()
+
+    def draw(self):
+        screen.blit("title", (0, 0))
+        anim_frame = min(9, (self.timer // 4) % 160)
+        screen.blit("space" + str(anim_frame), (0, 0))
